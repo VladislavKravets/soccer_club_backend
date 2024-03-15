61fb495e0087d292aa707b1dcbc865ec8b68d9be
vlad.kravets12 <vlad.kravets12@gmail.com>
2024-03-15 10:12:32 +0200

    add start message

--- src/main/java/com/example/soccer_club_backend/SoccerClubBackendApplication.java
+++ src/main/java/com/example/soccer_club_backend/SoccerClubBackendApplication.java
@@ -1,13 +1,16 @@
 package com.example.soccer_club_backend;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class SoccerClubBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SoccerClubBackendApplication.class, args);
-	}
 
+		log.info("App Was started!");
+	}
 }
